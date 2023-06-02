3567668e51aea4a40940d77a478b4808052c1dc5
Steven Tripari <07-bob.warble@icloud.com>
2023-06-01 22:00:30 -0400

    first commit

---
+++ src/store/reducers.js
@@ -0,0 +1,96 @@
+export const provider = (state = {}, action) => {
+  switch (action.type) {
+    case 'PROVIDER_LOADED':
+      return {
+        ...state,
+        connection: action.connection
+      }
+    case 'NETWORK_LOADED':
+      return {
+        ...state,
+        chainId: action.chainId
+      }
+    case 'ACCOUNT_LOADED':
+      return {
+        ...state,
+        account: action.account
+      }
+    case 'ETHER_BALANCE_LOADED':
+      return {
+        ...state,
+        balance: action.balance
+      }
+
+    default:
+      return state
+  }
+}
+
+const DEFAULT_TOKEN_STATE = {
+  loaded: false, 
+  contracts: [], 
+  symbols: []
+}
+
+export const tokens = (state = DEFAULT_TOKEN_STATE, action) => {
+  switch (action.type) {
+    case 'TOKEN_1_LOADED':
+      return {
+        ...state,
+        loaded: true,
+        contracts: [action.token],
+        symbols: [action.symbol]
+      }
+    case 'TOKEN_1_BALANCE_LOADED':
+      return {
+        ...state,
+        balances: [action.balance]
+      }
+
+    case 'TOKEN_2_LOADED':
+      return {
+        ...state,
+        loaded: true,
+        contracts: [...state.contracts, action.token],
+        symbols: [...state.symbols, action.symbol]
+      }
+
+    case 'TOKEN_2_BALANCE_LOADED':
+      return {
+        ...state,
+        balances: [action.balance]
+      }
+
+      default:
+        return state
+  }
+}
+
+export const exchange = (state = { loaded: false, contract: {} }, action) => {
+  switch (action.type) {
+    case 'EXCHANGE_LOADED':
+      return {
+        ...state,
+        loaded: true,
+        contracts: action.exchange
+      }
+      //---------------------------------------------------------------------
+      // Balance cases
+      case 'EXCHANGE_TOKEN_1_BALANCE_LOADED':
+      return {
+        ...state,
+        balances: [action.balance]
+      }
+      case 'EXCHANGE_TOKEN_2_BALANCE_LOADED':
+      return {
+        ...state,
+        balances: [...state.balances, action.balance]
+      }
+
+      default:
+        return state
+  }
+}
+
+
+
